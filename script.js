0375e6e6bc401110c926a398a2f691e07af4a630
Soo <li_xiuheng@hotmail.com>
2024-01-07 18:46:51 +0000

    finished first test, no error

--- script.js
+++ script.js
@@ -9,12 +9,6 @@ var hoursArray = []
 for (var i = 9; i < 23; i++) {
     hoursArray.push(i);
 
-$.each(hoursArray, function(i, val) {
-    const setHour = dayjs().hour(val);
-    var hourText = dayjs(setHour).format("h A");
-    $("#"+ val).text(hourText)
-})
-
 //define conditons for color difference in timeblock, using isBefore/isAfter in dayjs   
 if (dayjs().isAfter(dayjs().hour(i))) {
     $("#"+ i).next().css({"background-color": "#696969", "color": "#A9A9A9"})} //dimgrey,darkgry
@@ -22,31 +16,34 @@ if (dayjs().isAfter(dayjs().hour(i))) {
         $("#"+ i).next().css("background-color", "#FFA07A")} //lightsalmon
         else {
             $("#"+ i).next().css("background-color", "#98FB98")} //palegreen
-}
-//local storage: getItem of the container with free text
-//notification of item saved to local storage, may use function: after clicking the FA save button, notification appears
 
+$.each(hoursArray, function(i, val) {
 
-// var eventHour = document.querySelectorAll("span").value;
-// var eventItem = document.querySelectorAll(".form-control").value;
-// var eventList = {
-//     eventHour: eventHour,
-//     eventItem: eventItem
+    const setHour = dayjs().hour(val);
+    var hourText = dayjs(setHour).format("h A");
+    $("#"+ val).text(hourText);
+    $("#" + val).next().val();
+ $("button").click(function (event){
+    event.preventDefault();    
+//local storage: getItem of the container with free text if any
+    if ($("#" + val).next().val()) {
+   
+//notification of item saved to local storage, key using event hour    
+    localStorage.setItem(hourText, $("#" + val).next().val());
+    // var eventItem = $("#" + val).next().val();
+    // $("#" + val).next().text(eventItem);
+    // localStorage.getItem(hourText);
+   
+    $("#saved").fadeIn(1000);}});
+    $("#" + val).next().val(localStorage.getItem(hourText));
+    localStorage.getItem(hourText)
+} 
+    
+)
+    }
 
-$("button").click(function (event){
-    event.preventDefault();
-    // var textInput = $("#"+ i).val();
-    // var textVal = $("#"+ i).next().val();
-    localStorage.setItem($("#"+ i).val(), $("#"+ i).next().val());
-    var lsVal = localStorage.getItem($("#"+ i).val());
-    $(this).siblings("input").text(lsVal);
-    $("#saved").fadeIn(1000);
-   })
-
-setInterval(function fadeOut(){
+    setInterval(function fadeOut(){
     $("#saved").fadeOut()}
-    ,1000);
-
-
-
+    ,1000)
+        
 
