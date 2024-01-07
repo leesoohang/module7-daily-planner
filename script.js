5b058bbd26def1f726cce82234c5f7ba663ba2d4
Soo <li_xiuheng@hotmail.com>
2024-01-07 02:06:26 +0000

    tidied up variables

--- script.js
+++ script.js
@@ -2,69 +2,51 @@
 var currentDay = dayjs().format("dddd, MMM DD YYYY")
 $("#currentDay").text(currentDay)
 
-//current hour
-var currentHour = dayjs().hour()
 //set hour array to get all the hours for timeblock
 var hoursArray = []
 
-// function addText() {
+//use for loop and each function to add text based on the starting/ending hours with the desired format
 for (var i = 9; i < 23; i++) {
     hoursArray.push(i);
 
 $.each(hoursArray, function(i, val) {
-    var setHour = dayjs().hour(val)
-    var hourText = dayjs(setHour).format("h A")
+    const setHour = dayjs().hour(val);
+    var hourText = dayjs(setHour).format("h A");
     $("#"+ val).text(hourText)
-})}
+})
 
-
-// for (i = 9; i < 23; i++) {
-//     function hour(i) {
-//         var eventHour = dayjs().set('hour', i);
-//     $("span").text(eventHour)
-// }
-// }
-
-// var slot = dayjs(eventHour).format("h A")
-//set up objects var schedule containing hour slots and black containers to fill in
-
-// if hour in block < currentHour, timeblock background color gray
-
-// if (eventHour == currentHour) {
-//     $(".form-control").css("background-color", "#FA8072")}
-//     else if (eventHour < currentHour) {
-// $(".form-control").css("background-color", "#808080")}
-// // if the time in time block == currentHour, timeblock backgroud color red
-//         else {
-// // if hour in block > currentHour, timeblock background color green
-// $(".form-control").css("background-color", "#9ACD32")}
-
+//define conditons for color difference in timeblock, using isBefore/isAfter in dayjs   
+if (dayjs().isAfter(dayjs().hour(i))) {
+    $("#"+ i).next().css({"background-color": "#696969", "color": "#A9A9A9"})} //dimgrey,darkgry
+    else if (dayjs().isBefore(dayjs().hour(i))) {
+        $("#"+ i).next().css("background-color", "#FFA07A")} //lightsalmon
+        else {
+            $("#"+ i).next().css("background-color", "#98FB98")} //palegreen
+}
 //local storage: getItem of the container with free text
-
 //notification of item saved to local storage, may use function: after clicking the FA save button, notification appears
 
-$("button").click(function notification(){
+
+// var eventHour = document.querySelectorAll("span").value;
+// var eventItem = document.querySelectorAll(".form-control").value;
+// var eventList = {
+//     eventHour: eventHour,
+//     eventItem: eventItem
+
+$("button").click(function (event){
+    event.preventDefault();
+    // var textInput = $("#"+ i).val();
+    // var textVal = $("#"+ i).next().val();
+    localStorage.setItem($("#"+ i).val(), $("#"+ i).next().val());
+    var lsVal = localStorage.getItem($("#"+ i).val());
+    $(this).siblings("input").text(lsVal);
     $("#saved").fadeIn(1000);
-    })
+   })
 
 setInterval(function fadeOut(){
-    $("#saved").fadeOut()
-},1000)
+    $("#saved").fadeOut()}
+    ,1000);
 
 
-//set the hour slots using each()
 
 
-        
-//hours show on the page    
-
-
-        // $("span").attr("value", hoursArray[i])
-    
-    // function = $("span")[i].textContent
-
-
-    // $("span").text(eventHour[i]);
-
-    // function timeblock(i) {
-    //     dayjs().hour(hoursArray[i])
